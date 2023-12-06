11804ba7ea922a414045ed41406ab5a991ac8929
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-12-06 07:47:58 -0300

    Requisições a api e css globais

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+
+//components
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
       <div className='App'>
-        <nav id='navbar'>
-          <h2>
-            <Link to="/">Movies Lib</Link>
-          </h2>
-          <Link to="/movie/1">Movie</Link>
-          <Link to="/search">Search</Link>
-        </nav>
+        <Navbar/>
         <h2>Movies Lib</h2>
         <Outlet/>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY; 
 
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setTopMovies(data.results);
+  }
+
+  useEffect(() => {
+    const topRatedURL = `${moviesURL}top_rated?${apiKey}`
+    getTopRatedMovies(topRatedURL);
+  }, []);
+
   return (
-    <div>Home</div>
+    <div>
+      {topMovies && topMovies.map((item) => <p>{item.title}</p>)}
+    </div>
   )
 }
 
