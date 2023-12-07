defb562fa7e205518824b1fe85353834d531e4f4
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-12-07 07:57:09 -0300

    Melhorias css e Search page

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+
+import { FaStar } from 'react-icons/fa';
+const moviesImgURL = import.meta.env.VITE_IMG;
+const apiKey = import.meta.env.VITE_API_KEY; 
+
+const MovieCard = ({movie, showLink = true}) => {
+  return (
+    <div className='movie-card'>
+      <img src={`${moviesImgURL}${movie.poster_path}`} 
+          alt={movie.title} />
+      <h2>{movie.title}</h2>
+      <p>
+        <FaStar/> {movie.vote_average}
+      </p>
+      {showLink && <Link to={`movie/${movie.id}`}>Detalhes</Link>}
+    </div>
+  )
+}
+
+export default MovieCard
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react'
+import MovieCard from '../components/MovieCard';
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY; 
+
+import './MoviesGrid.css';
 
 const Home = () => {
   const [topMovies, setTopMovies] = useState([]);
@@ -16,8 +19,16 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      {topMovies && topMovies.map((item) => <p>{item.title}</p>)}
+    <div className='container'>
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length == 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 
+         && topMovies.map((item) => 
+                        <MovieCard 
+                          key={item.id}
+                          movie={item}/>)}
+      </div>
     </div>
   )
 }
