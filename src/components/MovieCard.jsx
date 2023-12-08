a17a120aecde9fac34a0cd43c361700762947994
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-12-08 07:47:14 -0300

    Apresentação individual detalhada de movie e css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,7 +13,7 @@ const MovieCard = ({movie, showLink = true}) => {
       <p>
         <FaStar/> {movie.vote_average}
       </p>
-      {showLink && <Link to={`movie/${movie.id}`}>Detalhes</Link>}
+      {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
     </div>
   )
 }
